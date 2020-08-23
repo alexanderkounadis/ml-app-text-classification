0c2426882679da8056ef7f477ca2e69186806dc4
Alexandros Kounadis <alexanderkounadis@gmail.com>
2020-08-23 18:38:58 +0300

    Project setup

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using MyMLAppML.Model;
+using System;
+
+namespace myMLApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Add input data
+            ModelInput input = new ModelInput();
+
+            input.SentimentText = "That is rude.";
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+
+            Console.WriteLine($"Text: {input.SentimentText}\nIs Toxic: {result.Prediction}");
+        }
+    }
+}
